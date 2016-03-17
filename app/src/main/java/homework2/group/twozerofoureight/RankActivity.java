4b790ea8af99573be793a602ecb643c21e7adc63
WeiJhe <sky820902@gmail.com>
2016-03-17 23:41:42 +0800

    modify Layout

--- app/src/main/java/homework2/group/twozerofoureight/RankActivity.java
+++ app/src/main/java/homework2/group/twozerofoureight/RankActivity.java
@@ -21,7 +21,7 @@ public class RankActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rank);
+        setContentView(R.layout.activity_rank_main);
         initViews();
     }
 
